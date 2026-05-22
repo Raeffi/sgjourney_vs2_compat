c0ca646829f95d469ca19b3b51a7e11446e72e94
Raeffi <raeffi@gmx.at>
2026-05-22 19:08:10 +0200

    Fixed Stuff

---
+++ src/main/java/raeffi/sgjourney_vs2_compat/sgjourney_vs2_compat.java
@@ -0,0 +1,36 @@
+package raeffi.sgjourney_vs2_compat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod("sgjourney_vs2_compat")
+public class sgjourney_vs2_compat {
+    public static final String MOD_ID = "sgjourney_vs2_compat";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public sgjourney_vs2_compat() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
